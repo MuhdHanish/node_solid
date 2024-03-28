cac3395c30af9374a1267727db000b76027a171a
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:18:44 +0530

    updated  comments in app file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express from 'express';
-import { config } from 'dotenv';
-import { router } from './routes';
+import express from "express"; 
+import { config } from "dotenv"; 
+import { router } from "./routes"; 
 
-config();
+// Load environment variables from .env file into process.env
+config(); 
 
-const app = express();
+// Create an Express application instance
+const app = express(); 
 
+// Middleware to parse JSON bodies of incoming requests
 app.use(express.json());
+
+// Middleware to use the router for handling routes
 app.use(router);
 
+// Export the Express application instance for use in other parts of the application
 export { app };
