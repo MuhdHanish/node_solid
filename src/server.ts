0b6382692def847e7fa0d491e36e0c4c9c7db5c9
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:19:26 +0530

    updated  comments in server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import { app } from "./app";
+import { app } from "./app"; 
 
+// Define the port on which the server will listen for incoming requests
 const PORT = process.env.PORT || 8000;
 
+// Start the Express server, listening on the specified port
 app.listen(PORT, () => {
+    // Log a message indicating that the server is running and on which port
     console.log(`Server is running on port ${PORT}`);
 });
