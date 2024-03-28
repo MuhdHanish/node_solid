dd703048adf287c0976b5b7c7e0cb5d90bc802b9
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:13:08 +0530

    added comments in create user controller

--- src/useCases/CreateUser/CreateUserController.ts
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from "express";
+import { ICreateUserRequestDTO } from "./CeateUserDTO"; 
 import { CreateUserUseCase } from "./CreateUserUseCase";
-import { ICreateUserRequestDTO } from "./CeateUserDTO";
 
+// Controller responsible for handling user creation requests
 export class CreateUserController {
     constructor(
+        // Instance of CreateUserUseCase
         private readonly createUserUseCase: CreateUserUseCase
     ) { }
 
+    // Method to handle incoming HTTP requests to create a new user
     async handle(req: Request, res: Response): Promise<Response> {
         try {
+            // Extract user data from the request body
             const userData: ICreateUserRequestDTO = req.body;
+
+            // Execute the use case to create a new user using the provided data
             await this.createUserUseCase.execute(userData);
+
+            // Respond with success message if user creation is successful
             return res.status(201).send({ message: "User created successfully." });
         } catch (error) {
+            // Handle errors during user creation
             console.error("Error creating user:", error);
+
+            // Respond with an error message and status code 500 (Internal Server Error)
             return res.status(500).send({ error: "Internal Server Error.", message: error instanceof Error ? error.message : `Unexpected Error.` });
         }
     }
