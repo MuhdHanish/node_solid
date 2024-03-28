524fb2bd192c2871f8274e4feee69ab769888b0b
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:08:16 +0530

    added comments in create user request DTO interface

--- src/useCases/CreateUser/CeateUserDTO.ts
+++ src/useCases/CreateUser/CeateUserDTO.ts
@@ -1,5 +1,6 @@
+// Interface defining the structure of data required to create a new user
 export interface ICreateUserRequestDTO {
-    name: string;
-    email: string;
-    password: string;
+    name: string;     // Name of the user
+    email: string;    // Email address of the user
+    password: string; // Password of the user
 }
