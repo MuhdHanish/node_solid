9ce59702ad3ccc3710f45dec1c105548cf172828
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:04:27 +0530

    added comments in mail provider  interface

--- src/providers/IMailProvider.ts
+++ src/providers/IMailProvider.ts
@@ -1,15 +1,19 @@
-interface IAddress {
-    email: string;
-    name: string;
+// Interface defining the structure of an email address
+export interface IAddress {
+    email: string; // Email address of the user
+    name: string;  // Name of the user
 }
 
+// Interface defining the structure of an email message
 export interface IMessage {
-    to: IAddress;
-    from: IAddress;
-    subject: string;
-    body: string;
+    to: IAddress;       // Recipient of the email
+    from: IAddress;     // Sender of the email
+    subject: string;    // Subject of the email
+    body: string;       // Body/content of the email
 }
 
-export interface IMailProvier {
+// Interface defining methods for sending emails
+export interface IMailProvider {
+    // Send an email message
     sendMail(message: IMessage): Promise<void>;
 }
