55a9e812d7c8a2a58f5386835f4900dd0e4194b5
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:01:48 +0530

    added comments in local users repostory implementation

--- src/repositories/implementations/LocalUsersRepository.ts
+++ src/repositories/implementations/LocalUsersRepository.ts
@@ -2,18 +2,18 @@ import { User } from "../../entities/User";
 import { IUsersRepository } from "../IUsersRepository";
 
 export class LocalUsersRepository implements IUsersRepository {
-    private users: User[];
+    // Initialize the users array
+    private users: User[] = [];
 
-    constructor() {
-        this.users = []; 
-    }
-
+    // Find a user by their email address
     async findByEmail(email: string): Promise<User | undefined> {
+        // Search for the user in the users array
         const user = this.users.find(user => user.email === email);
-        return user;
+        return user; // Return the user if found, otherwise return undefined
     }
 
+    // Save a user to the local storage (array)
     async save(user: User): Promise<void> {
-        this.users.push(user);
+        this.users.push(user); // Add the user to the users array
     }
 }
