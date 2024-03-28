ebcc7d375ce00929e84ae0b2a6caf413f7c6caf8
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 13:53:58 +0530

    changed uuidv4 to uuid package

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,8 +1,7 @@
-import { uuid } from "uuidv4";
+import { v4 as uuid } from "uuid";
 
 export class User {
     public readonly id!: string;
-    
     public name!: string;
     public email!: string;
     public password!: string;
