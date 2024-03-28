8a06e37701b43091b9a06fb5e506871017d6517a
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:16:34 +0530

    added comments in module of user Create User contrller

--- src/useCases/CreateUser/index.ts
+++ src/useCases/CreateUser/index.ts
@@ -1,18 +1,26 @@
 import { CreateUserUseCase } from "./CreateUserUseCase";
 import { CreateUserController } from "./CreateUserController";
-import { GmailMailProvider } from "../../providers/implementations/GmailMailProvider";
+import { GmailMailProvider } from "../../providers/implementations/GmailMailProvider"; 
 import { LocalUsersRepository } from "../../repositories/implementations/LocalUsersRepository";
 
+// Create instances of the required classes and interfaces
+
+// Create a new instance of User Repository to handle user data storage
 const userRepository = new LocalUsersRepository();
+
+// Create a new instance of Mail Provider to handle sending emails
 const mailProvider = new GmailMailProvider();
 
+// Create a new instance of Use Case, injecting the user repository and mail provider
 const createUserUseCase = new CreateUserUseCase(
     userRepository,
     mailProvider
 );
 
+// Create a new instance of Controller, injecting the Use Case
 const createUserController = new CreateUserController(
     createUserUseCase
 );
 
+// Export instances of Use Case and Controller for use in other parts of the application
 export { createUserUseCase, createUserController };
