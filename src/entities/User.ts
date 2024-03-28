1ac499b7b57d254790426f3f8f7475b71f6993ec
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:07:29 +0530

    added comments in User entitie

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,13 +1,18 @@
 import { v4 as uuid } from "uuid";
 
+// User class representing a user entity
 export class User {
-    public readonly id!: string;
-    public name!: string;
-    public email!: string;
-    public password!: string;
+    public readonly id!: string; // Unique identifier for the user
+    public name!: string;        // Name of the user
+    public email!: string;       // Email address of the user
+    public password!: string;    // Password of the user
 
+    // Constructor to create a new User object
     constructor(props: Omit<User, 'id'>, id?: string) {
+        // Assign properties passed in props to the User object
         Object.assign(this, props);
+
+        // If id is not provided, generate a new unique id using uuid
         if (!id) {
             this.id = uuid();
         }
