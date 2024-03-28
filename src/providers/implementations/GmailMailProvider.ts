accb00dc718e8a536b012cebb7c4cee1308cbd38
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:06:26 +0530

    added comments in Gmail Mail provider

--- src/providers/implementations/GmailMailProvider.ts
+++ src/providers/implementations/GmailMailProvider.ts
@@ -1,33 +1,41 @@
 import Mail from "nodemailer/lib/mailer";
 import nodemailer from 'nodemailer';
-import { IMailProvier, IMessage } from "../IMailProvider";
+import { IMailProvider, IMessage } from "../IMailProvider";
 
-
-export class GmailMailProvider implements IMailProvier {
+// Implementation of the IMailProvider interface using Gmail SMTP
+export class GmailMailProvider implements IMailProvider {
     private transporter: Mail;
 
+    // Constructor to initialize the transporter with Gmail SMTP settings
     constructor() {
+        // Create a Nodemailer transporter using Gmail SMTP settings
         this.transporter = nodemailer.createTransport({
-            service: `gmail`,
+            service: `gmail`, // Use Gmail service
             auth: {
-                user: 'example@mail.com',
-                pass: 'onetimepasswordofmail'
+                user: 'example@mail.com', // Your Gmail email address
+                pass: 'example.password' // Your One Time Password For Sending Email
             }
-        })
+        });
     }
 
+    // Method to send an email message using Gmail SMTP
     async sendMail(message: IMessage): Promise<void> {
+        // Send the email message using the configured transporter
         await this.transporter.sendMail({
+            // Set the recipient's name and email address
             to: {
                 name: message.to.name,
                 address: message.to.email
             },
+            // Set the sender's name and email address
             from: {
                 name: message.from.name,
                 address: message.from.email
             },
+            // Set the subject of the email
             subject: message.subject,
+            // Set the HTML body/content of the email
             html: message.body
-        })
+        });
     }
 }
