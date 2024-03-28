866683f011cf756d061ce5625687ee82e40655f5
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-28 14:03:33 +0530

    added comments in users repostory interface

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -1,6 +1,10 @@
 import { User } from "../entities/User";
 
+// Interface defining methods to interact with user data in a repository
 export interface IUsersRepository {
+    // Find a user by their email address
     findByEmail(email: string): Promise<User | undefined>;
+
+    // Save a user to the storage
     save(user: User): Promise<void>;
 }
