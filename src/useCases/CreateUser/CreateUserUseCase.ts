d726bc197e03f4ae19b1fe548e9328b22aece64f
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-14 11:08:59 +0530

    refactor: removed mail sending while creating user

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -27,31 +27,5 @@ export class CreateUserUseCase {
 
         // Save the new user to the repository
         await this.userRepository.save(user);
-
-        // Send a welcome email to the newly created user
-        this.mailProvider.sendMail({
-            to: {
-                email: userData.email,
-                name: userData.name,
-            },
-            from: {
-                email: process.env.SENDER_EMAIL || `sender@example.com`,
-                name: process.env.SENDER_NAME || `Sender Name`,
-            },
-            subject: `Welcome To SOLID Node TS Server`,
-            body: `
-                <h1>SOLID NODE TS SERVER</h1>
-                <p>Dear ${userData.name},</p>
-                <p>Welcome To SOLID Node TS Server! 🚀</p>
-                <p>Here's a brief overview of our platform:</p>
-                <ul>
-                    <li>We follow SOLID principles to ensure our codebase is maintainable and scalable.</li>
-                    <li>Our backend is built using Node.js with TypeScript, providing type safety and enhanced developer productivity.</li>
-                    <li>We adhere to Clean Architecture principles to keep our codebase organized and maintainable, enabling easier testing and future enhancements.</li>
-                </ul>
-                <p>Once again, welcome aboard!</p>
-                <p>Best regards,<br>${process.env.SENDER_NAME || `[Your Name]`}</p>
-            `,
-        });
     }
 }
