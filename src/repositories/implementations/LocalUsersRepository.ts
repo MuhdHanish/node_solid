cf4fc9e49232d4053f6e25bcc6dcf65497c9fc56
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-14 11:08:31 +0530

    feat: implmented getUsers func in localUsersRepository

--- src/repositories/implementations/LocalUsersRepository.ts
+++ src/repositories/implementations/LocalUsersRepository.ts
@@ -3,17 +3,22 @@ import { IUsersRepository } from "../IUsersRepository";
 
 export class LocalUsersRepository implements IUsersRepository {
     // Initialize the users array
-    private users: User[] = [];
+    private static users: User[] = [];
 
     // Find a user by their email address
     async findByEmail(email: string): Promise<User | undefined> {
         // Search for the user in the users array
-        const user = this.users.find(user => user.email === email);
+        const user = LocalUsersRepository.users.find(user => user.email === email);
         return user; // Return the user if found, otherwise return undefined
     }
 
     // Save a user to the local storage (array)
     async save(user: User): Promise<void> {
-        this.users.push(user); // Add the user to the users array
+        LocalUsersRepository.users.push(user); // Add the user to the users array
+    }
+
+    // Find all users
+    async getUsers(): Promise<User[]>{
+        return LocalUsersRepository.users;
     }
 }
